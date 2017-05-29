105a0d735fa36a98afe6e8d8a30748e11c80da9b
pourzanj <arya@umail.ucsb.edu>
2017-05-29 13:23:00 -0700

    rewrote sample derivs to take a posterior predtive sample of derivative of only one state at a time instead of weird Gaussian process that handled 4 states at a time. Also functionalized a couple functions, most notably the Stanfits so we can use mclapply to run in parallel.

--- pendulum_fit.R
+++ pendulum_fit.R
@@ -1,9 +1,12 @@
 library(MASS)
 library(tidyverse)
 library(rstan)
+options(mc.cores = parallel::detectCores())
 library(ggplot2)
 library(deSolve)
 library(GGally)
+library(purrr)
+library(parallel)
 
 setwd("~/gp")
 
@@ -33,30 +36,118 @@ dft = df %>% filter(type == "full")
 
 df %>% ggplot(aes(time, y)) +
   geom_line(aes(colour = type)) +
-  geom_point(aes(time, ynoise, colour = type))
+  geom_point(aes(time, ynoise, colour = type), size = 0.5)
 
-# Find GP hyperparameters
+####################################################
+####################################################
+# For a process observed with noise at a finite number of points,
+# Find the hyperparameters of the SEXP kernel describing it.
+# By find we mean get draws from the posterior of these hyperparameters
+####################################################
+####################################################
 {
   sdata = list(N = nrow(dft),
                t = dft$time,
                y = dft$ynoise)
   
-  fit = stan("/home/bbales2/gp/models/fit_hyperparameters.stan", data = sdata, cores = 4, iter = 1000)
+  fit = stan("models/fit_hyperparameters.stan", data = sdata, cores = 4, iter = 1000)
   
   s = extract(fit, pars = c("rho", "alpha", "sigma"))
 }
 
+####################################################
+####################################################
+# once we get posterior samples of the hyperparameters we can sample from them
+# to generate posterior predictive draws of the derivative of the original process.
+####################################################
+####################################################
+{
+  #function that takes single posterior sample of rho, alpha, and sigma and returns
+  #a single posterior draw of the derivative process (w/o noise)
+  sample_derivs = function(params, ynoise, ti) {
+    l <- params[1]
+    a <- params[2]
+    sy <- params[3]
+    
+    N = length(ynoise)
+
+    #use outer to create kernel evaluated at all combinations of time points.
+    #the original kernel function take in two time poitns AND an l so we use
+    #kern_fixed_l to make versions of the kernel function w/ a fixed l
+    kern_fixed_l <- function(kern, l) {function(tj, tk) kern(tj, tk, l)} 
+    K <- a^2 * outer(ti, ti, FUN = kern_fixed_l(QQ, l))    #cov between old points and old points
+    KsK <- a^2 * outer(ti, ti, FUN = kern_fixed_l(RQ, l))  #old points and new points
+    KsKs <- a^2 * outer(ti, ti, FUN = kern_fixed_l(RR, l)) #new points and new points
+    
+    build_mu = function(K, KsK, y) {
+      diag(K) = diag(K) + sy^2
+      KsK %*% solve(K, ynoise)
+    }
+    
+    build_cov = function(K, KsK, KsKs) {
+      KKs = t(KsK)
+      diag(K) = diag(K) + sy^2
+      KsKs - KsK %*% solve(K, KKs) + diag(1e-8, nrow(K), ncol(K))
+    }
+    
+    out = mvrnorm(1, build_mu(K, KsK, ynoise), build_cov(K, KsK, KsKs))
+    return(out)
+  }
+  
+  #get all of our posterior samples as a list of vectors c(rho, alpha, sigma)
+  get_single_sample = function(i) c(rho = s$rho[i], alpha = s$alpha[i], sigma = s$sigma[i])
+  s_list = map(1:500, get_single_sample)
+  
+  #for each posterior sample of c(rho, alpha, sigma) get a post. pred. sample of deriv. for both states
+  sample_derivs_both_states <- function(params, ynoise, ypnoise, ti) {
+    return(list(yp_hat = sample_derivs(params, ynoise, ti),
+                ypp_hat = sample_derivs(params, ypnoise, ti)))
+  }
+  post_draws <- mclapply(s_list, sample_derivs_both_states, ynoise = dft$ynoise, ypnoise = dft$ypnoise, ti = dft$time, mc.cores = 2)
+  
+  #create a function that does a Stan fit given a list containing yp_hat and ypp_hat
+  sf = stan_model("models/fit_ypypp.stan")
+  
+  get_stan_fit_from_impute <- function(imputed_draw) {
+    sdata = list(N = nrow(dft),
+                 t = dft$time,
+                 y = dft$ynoise,
+                 yp = dft$ypnoise,
+                 yph = imputed_draw$yp_hat,
+                 ypph = imputed_draw$ypp_hat)
+    
+    fit <- sampling(sf, data = sdata, chains = 1, cores = 1, iter = 1000)
+    return(fit)
+  }
+  
+  #apply function to get a single Stan fit for each posterior predictive draw all in one big list
+  stan_fits <- mclapply(post_draws, get_stan_fit_from_impute, mc.cores = 4)
+  
+  #coalesce samples from these stan fits in to a single dataframe
+  df_impute2 = bind_rows(lapply(stan_fits, function(x) {
+    extract(x, pars = c("a", "b", "c", "d", "sigmay", "sigmayp"))
+  })) %>% sample_frac(1.0)
+  
+  #pairs plot
+  df_impute2 %>% sample_n(2000) %>% ggpairs()
+}
+
+####################################################
+####################################################
 # Impute data and fit with linearized system
+####################################################
+####################################################
 {
   sample_derivs = function(l, a, sy) {
     N = nrow(dft)
     ti = dft$time
     y = c(dft$ynoise, dft$ypnoise)
     
-    K = matrix(0, 2 * N, 2 * N)
-    KsK = matrix(0, 2 * N, 2 * N)
-    KsKs = matrix(0, 2 * N, 2 * N)
+    K = matrix(0, 2 * N, 2 * N)      #cov between old points and old points
+    KsK = matrix(0, 2 * N, 2 * N)    #old points and new points
+    KsKs = matrix(0, 2 * N, 2 * N)   #new points and new points
     
+    #
     for(jt in 1:N) {
       for(kt in 1:N) {
         K[jt, kt] = a^2 * QQ(ti[jt], ti[kt], l)
