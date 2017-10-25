37431536c1c0a355c3fc381923394ff1f5092986
bbbales2 <bbbales2@gmail.com>
2017-10-24 17:16:47 -0700

    Couple updates

--- gpc_sigma.R
+++ gpc_sigma.R
@@ -8,32 +8,35 @@ sourceCpp("covariance.cpp")
 
 M = 11 # Order of GPC expansion
 K = 15 # Order of polynomial expansions for integration
+k = 1.0
+alpha = k - 1.0
 
 x = seq(0.0, 1.0, length = 3)
 func = function(l) {
-  c(chol(rbf_cov(x, x, l)))
+  sin(2 * l)#c(chol(rbf_cov(x, x, l)))
 }
 
-quad = laguerre_integrate(K)
+quad = laguerre_integrate(K, alpha)
 
 polys = list()
 lookupt = list()
 for(m in 0:M) {
-  polys[[m + 1]] = laguerre_integrate(m)$polynomial
+  polys[[m + 1]] = laguerre_integrate(m, alpha)$polynomial
   output = map(quad$roots, func)
   for(i in 1:length(output)) {
-    output[[i]] = output[[i]] * quad$weights[[i]] * polynomial.values(polys[m + 1], quad$roots[[i]])[[1]]
+    output[[i]] = output[[i]] *
+      quad$weights[[i]] *
+      polynomial.values(polys[m + 1], quad$roots[[i]])[[1]]
   }
-  
+
   lookupt[[m + 1]] = Reduce('+', output)
 }
 
-xl = 1.5
 rbf_cov_approx = function(xl) {
   Reduce('+', lapply(0:M, function(m) { lookupt[[m + 1]] * polynomial.values(polys[m + 1], xl)[[1]] }))
 }
 
-list(lvs = seq(0.01, 5.0, length = 100)) %>% as.tibble %>%
+list(lvs = rgamma(100, k)) %>% as.tibble %>%
   mutate(approx = map(c(lvs), rbf_cov_approx),
          exact = map(c(lvs), func)) %>% unnest() %>%
   gather(which, y, c(-lvs)) %>%
